10983dd04b4e457c7663f26647f5cd9b3792d88a
mazenswar <mazenswar@protonmail.com>
2019-02-07 14:25:35 -0500

    costume store

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,32 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :haunted_houses do |i|
+      i.string :name
+      i.string :location
+      i.string :theme
+      i.float :price
+      i.boolean :family_friendly
+      i.datetime :opening_date
+      i.datetime :closing_date
+      i.string :description
+    end
+
+  end
+###
+end
+
+
+#
+# The haunted_houses table will have eight columns:
+#
+# name
+# location
+# theme
+# price
+# whether they're family friendly or not
+# opening date
+# closing date
+# long description
